ce878073e021db9eea63ae2a337276547f0ab9c6
Penelope Yong <penelopeysm@gmail.com>
2025-03-26 14:01:41 +0000

    More updates

--- src/models.jl
+++ src/models.jl
@@ -7,10 +7,30 @@ export MODELS
 
 @model function demo_dynamic_constraint()
     m ~ Normal()
-    x ~ truncated(Normal(), m, Inf)
+    x ~ truncated(Normal(); lower=m)
     return (m = m, x = x)
 end
 
-MODELS = [demo_dynamic_constraint()]
+MODELS = Model[demo_dynamic_constraint()]
+
+# Add models with different distributions
+
+DISTRIBUTIONS = Dict(
+    :normal => Normal(),
+    :inverse_gamma => InverseGamma(),
+    :beta => Beta(2, 2),
+    :lkjcholu => LKJCholesky(5, 1.0, 'U'),
+    :lkjcholl => LKJCholesky(5, 1.0, 'L'),
+    :wishart => Wishart(7, [1.0 0.5; 0.5 1.0]),
+)
+
+for (name, distribution) in DISTRIBUTIONS
+    @eval begin
+        @model function $name()
+            m ~ $distribution
+        end
+        push!(MODELS, $name())
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,7 @@ import Enzyme: set_runtime_activity, Forward, Reverse, ForwardMode, ReverseMode
 List of AD backends to test.
 """
 ADTYPES = [
+    AutoForwardDiff(),
     AutoReverseDiff(; compile=false),
     AutoReverseDiff(; compile=true),
     AutoMooncake(; config=nothing),
@@ -45,95 +46,141 @@ get_adtype_shortname(::AutoEnzyme{<:ReverseMode}) = "Enzyme:Reverse"
 get_adtype_shortname(::AutoEnzyme{<:ForwardMode}) = "Enzyme:Forward"
 
 """
-    test_correctness(
-        model::Model,
-        adtypes::Vector{<:ADTypes.AbstractADType},
-        reference_adtype::ADTypes.AbstractADType,
-        rng::Random.AbstractRNG=Xoshiro(468),
-        params::Vector{<:Real}=VarInfo(rng, model)[:];
-        value_atol=1e-6,
-        grad_atol=1e-6
-    )
+    ADTestResult
 
-Test the correctness of all the AD backend `adtypes` for the model `model` The
-test is performed by calculating the logdensity and its gradient using all the
-AD backends, and comparing the results against that obtained with the reference
-AD backend `reference_adtype`.
+Data structure to store the results of the AD correctness test.
 
-The parameters can either be passed explicitly using the `params` argument, or can
-be sampled from the prior distribution of the model using the `rng` argument.
+If you want to quickly check whether the result is a success or failure, you
+can use `isnothing(result.error)`.
+
+$(TYPEDFIELDS)
 """
-function test_correctness(
+struct ADTestResult
+    "The DynamicPPL model that was tested"
+    model::Model, "The values at which the model was evaluated"
+    params::Vector{<:Real}, "The AD backend that was tested"
+    adtype::ADTypes.AbstractADType,
+    "The absolute tolerance for the value of logp"
+    value_atol::Real,
+    "The absolute tolerance for the gradient of logp"
+    grad_atol::Real,
+    "If the test ran, the expected value of logp (calculated using the reference AD backend)"
+    value_expected::Union{Nothing,Real},
+    "If the test ran, the expected gradient of logp (calculated using the reference AD backend)"
+    grad_expected::Union{Nothing,Vector{<:Real}},
+    "If the test ran, the actual value of logp (calculated using `adtype`)"
+    value_actual::Union{Nothing,Real},
+    "If the test ran, the actual gradient of logp (calculated using `adtype`)"
+    grad_actual::Union{Nothing,Vector{<:Real}},
+    "If the test ran and benchmarking was requested, the time taken by the AD backend to calculate the gradient of logp, divided by the time taken to evaluate logp itself"
+    time_vs_primal::Union{Nothing,Real},
+    "If the test did not run successfully, the error that was thrown"
+    error::Union{Nothing,Exception}
+end
+
+struct ADIncorrectException <: Exception
+end
+
+"""
+    run_ad(
+        model::Model,
+        adtype::ADTypes.AbstractADType;
+        benchmark=false,
+        value_atol=1e-6,
+        grad_atol=1e-6,
+        varinfo::AbstractVarInfo=VarInfo(model),
+        params::Vector{<:Real}=varinfo[:],
+        reference_adtype::ADTypes.AbstractADType=REFERENCE_ADTYPE,
+        expected_value_and_grad::Union{Nothing,Tuple{Real,Vector{<:Real}}}=nothing,
+    )::ADTestResult
+
+Test the correctness of the AD backend `adtype` for the model `model`. If
+`benchmark` is set to `true`, also benchmark the AD backend. By default this is
+`false`.
+
+Returns an [`ADTestResult`](@ref) object, which contains the results of the
+test and/or benchmark.
+
+The signature of this function is complicated. There are two things that must
+be provided:
+
+1. `model` - The model being tested.
+2. `adtype` - The AD backend being tested.
+
+Everything else is optional, and can be categorised into several groups:
+
+1. _How to specify the VarInfo._ DynamicPPL contains several different types of
+VarInfo objects which change the way model evaluation occurs. If you want to use
+a specific type of VarInfo, pass it as the `varinfo` argument. Otherwise, it will
+default to using a `TypedVarInfo` generated from the model.
+
+2. _How to specify the parameters._ For maximum control over this, generate a
+vector of parameters yourself and pass this as the `params` argument. If you
+don't specify this, it will be taken from the contents of the VarInfo. Note that
+if the VarInfo is not specified (and thus automatically generated) the parameters
+in it will have been sampled from the prior of the model.
+
+3. _How to specify the results to compare against._ Once logp and its gradient
+has been calculated with the specified `adtype`, it must be tested for correctness.
+This can be done either by specifying `reference_adtype`, in which case logp
+and its gradient will also be calculated with this reference in order to obtain
+the ground truth; or by using `expected_value_and_grad`, which is a tuple of
+(logp, gradient) that the calculated values must match. The latter is useful if
+you are testing multiple AD backends and want to avoid recalculating the ground
+truth multiple times.
+
+4. _How to specify the tolerances._ The tolerances for the value and gradient can
+be set using `value_atol` and `grad_atol`. These default to 1e-6.
+"""
+function run_ad(
     model::Model,
-    adtypes::Vector{<:ADTypes.AbstractADType},
-    reference_adtype::ADTypes.AbstractADType,
-    rng::Random.AbstractRNG=Xoshiro(468),
-    params::Vector{<:Real}=VarInfo(rng, model)[:];
+    adtype::ADTypes.AbstractADType;
     value_atol=1e-6,
     grad_atol=1e-6,
-)
-    ldf_reference = LogDensityFunction(model; adtype=reference_adtype)
-    value_true, grad_true = LogDensityProblems.logdensity_and_gradient(ldf_reference, params)
-    info_str = join(
-        [
-            "Reference backend calculation: $(model.f), $(get_adtype_shortname(reference_adtype))",
-            "       params : $(params)",
-            "    logp,grad : $((value_true, grad_true))",
-        ],
-        "\n",
-    )
-    @info info_str
+    rng::Random.AbstractRNG=Xoshiro(468),
+    varinfo::AbstractVarInfo=VarInfo(rng, model),
+    params::Vector{<:Real}=varinfo[:],
+    reference_adtype::ADTypes.AbstractADType=REFERENCE_ADTYPE,
+    expected_value_and_grad::Union{Nothing,Tuple{Real,Vector{<:Real}}}=nothing,
+)::ADTestResult
+    try
+        # Calculate ground truth to compare against
+        value_true, grad_true = if expected_value_and_grad === nothing
+            ldf_reference = LogDensityFunction(model; adtype=reference_adtype)
+            LogDensityProblems.logdensity_and_gradient(ldf_reference, params)
+        else
+            expected_value_and_grad
+        end
 
-    for adtype in adtypes
         ldf = LogDensityFunction(model; adtype=adtype)
         value, grad = LogDensityProblems.logdensity_and_gradient(ldf, params)
-        info_str = join(
-            [
-                "Testing correctness: $(model.f), $(get_adtype_shortname(adtype))",
-                "       params : $(params)",
-                "       actual : $((value, grad))",
-                "     expected : $((value_true, grad_true))",
-            ],
-            "\n",
-        )
+        info_str = "`run_ad` on $(model.f), $(adtype)\n" *
+                   "       params : $(params)\n" *
+                   "       actual : $((value, grad))\n" *
+                   "     expected : $((value_true, grad_true))\n"
         @info info_str
-        @test value ≈ value_true atol = value_atol
-        @test grad ≈ grad_true atol = grad_atol
+        value_is_correct = isapprox(value, value_true; atol=value_atol)
+        grad_is_correct = isapprox(grad, grad_true; atol=grad_atol)
+
+        exc = if value_is_correct && grad_is_correct
+            nothing
+        else
+            ADIncorrectException()
+        end
+
+        return ADTestResult(
+            model, params, adtype, value_atol, grad_atol,
+            value_true, grad_true, value, grad, nothing, exc
+        )
+    catch e
+        return ADTestResult(
+            model, params, adtype, value_atol, grad_atol,
+            nothing, nothing, nothing, nothing, nothing, e
+        )
     end
 end
 
-@testset "$(model.f)" for model in MODELS
+@testset verbose = true "$(model.f)" for model in MODELS
+    @info "Testing AD correctness for $(model.f)"
     test_correctness(model, ADTYPES, REFERENCE_ADTYPE)
 end
-
-# # Alternatively, we can test these manually with DifferentiationInterfaceTest,
-# # and even benchmark them.
-# import ModelTests: make_function, make_params
-# import DifferentiationInterfaceTest:
-#     test_differentiation, benchmark_differentiation, Scenario
-# import DataFrames: select, subset, ByRow
-# function make_scenario(model)
-#     f = make_function(model)
-#     x = make_params(model)
-#     true_grad = ad_ldp(model, x, REFERENCE_ADTYPE)[2]
-#     Scenario{:gradient,:out}(f, x; res1=true_grad)
-# end
-# function get_scenario_model_function_name(scenario)
-#     # Hacky way to extract the model name from the scenario
-#     re = r"Model{typeof\(([^.)]+\.)*([^.)]+)\),"
-#     String(match(re, string(scenario))[2])
-# end
-# SCENARIOS = [make_scenario(model) for model in MODELS]
-# test_differentiation(ADTYPES, SCENARIOS)
-#
-# df = benchmark_differentiation(ADTYPES, SCENARIOS)
-# # Clean the dataframe a bit, otherwise it's impossible to read
-# df = subset(df, :operator => x -> x .== :gradient)
-# df = select(
-#     df,
-#     :scenario => ByRow(get_scenario_model_function_name) => :model,
-#     :backend => ByRow(get_adtype_shortname) => :backend,
-#     :time => ByRow(x -> x * 1e6) => Symbol("time/μs"),
-#     :allocs,
-# )
-# println(df)
