4fa85e537a5834dcc624cf84ff2d20cb734425dc
Penelope Yong <penelopeysm@gmail.com>
2025-04-07 02:04:09 +0100

    3dp

--- app/output.jl
+++ app/output.jl
@@ -4,6 +4,7 @@ Pkg.develop(; path=joinpath(@__DIR__, ".."))
 import Test: @test, @testset
 import ModelTests: MODELS, run_ad, ADIncorrectException
 using ADTypes
+using Printf: @printf
 
 import ForwardDiff
 import ReverseDiff
@@ -20,10 +21,10 @@ NOTE: Make sure that the names are unique and do not contain commas
 ADTYPES = Dict(
     "ForwardDiff" => AutoForwardDiff(),
     "ReverseDiff" => AutoReverseDiff(; compile=false),
-    # "ReverseDiff:Compiled" => AutoReverseDiff(; compile=true),
-    # "Mooncake" => AutoMooncake(; config=nothing),
-    # "Enzyme:Forward" => AutoEnzyme(; mode=set_runtime_activity(Forward, true)),
-    # "Enzyme:Reverse" => AutoEnzyme(; mode=set_runtime_activity(Reverse, true)),
+    "ReverseDiff:Compiled" => AutoReverseDiff(; compile=true),
+    "Mooncake" => AutoMooncake(; config=nothing),
+    "Enzyme:Forward" => AutoEnzyme(; mode=set_runtime_activity(Forward, true)),
+    "Enzyme:Reverse" => AutoEnzyme(; mode=set_runtime_activity(Reverse, true)),
 )
 
 if ARGS == ["--list-model-keys"]
@@ -34,7 +35,7 @@ elseif length(ARGS) == 3 && ARGS[1] == "--run"
     model, adtype = MODELS[ARGS[2]], ADTYPES[ARGS[3]]
     result = run_ad(model, adtype; benchmark=true)
     if isnothing(result.error)
-        println(result.time_vs_primal)
+        @printf("%.3f", result.time_vs_primal)
     elseif result.error isa ADIncorrectException
         println("incorrect")
     else
