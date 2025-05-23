4963141643007f7302fff1dfd71a4fd889e4bb7b
Penelope Yong <penelopeysm@gmail.com>
2025-05-23 12:01:43 +0100

    Minimised Enzyme

--- main.jl
+++ main.jl
@@ -1,127 +1,35 @@
-import Test: @test, @testset
-using DynamicPPL: DynamicPPL, VarInfo
-using DynamicPPL.TestUtils.AD: run_ad, ADResult, ADIncorrectException
-using ADTypes
-using Printf: @printf
+module MWE
 
-import FiniteDifferences: central_fdm
-import ForwardDiff
-import ReverseDiff
-import Mooncake
-import Enzyme: set_runtime_activity, Forward, Reverse
-import Zygote
+import Enzyme: Enzyme, Reverse, Const, set_runtime_activity
 
-# AD backends to test.
-ADTYPES = Dict(
-    "FiniteDifferences" => AutoFiniteDifferences(; fdm = central_fdm(5, 1)),
-    "ForwardDiff" => AutoForwardDiff(),
-    "ReverseDiff" => AutoReverseDiff(; compile = false),
-    "ReverseDiffCompiled" => AutoReverseDiff(; compile = true),
-    "Mooncake" => AutoMooncake(; config = nothing),
-    "EnzymeForward" => AutoEnzyme(; mode = set_runtime_activity(Forward, true)),
-    "EnzymeReverse" => AutoEnzyme(; mode = set_runtime_activity(Reverse, true)),
-    "Zygote" => AutoZygote(),
-)
+Enzyme.Compiler.VERBOSE_ERRORS[] = true
 
-# Models to test. The convention is that:
-#    a, b, c, ... are assumed variables
-#    x, y, z, ... are observed variables
-# although it's hardly a big deal.
-MODELS = Dict{String,DynamicPPL.Model}()
-macro register(model)
-    :(MODELS[string($(esc(model)).f)] = $(esc(model)))
+struct PStruct end
+Base.getproperty(::Type{PStruct}, s::Symbol) = getfield(PStruct, s)
+struct QStruct end
+Base.getproperty(::Type{QStruct}, s::Symbol) = getfield(QStruct, s)
+struct RStruct end
+Base.getproperty(::Type{RStruct}, s::Symbol) = getfield(RStruct, s)
+
+function g(xs)
+    return broadcast(x -> (exp(x), x), xs)
 end
 
-# These imports tend to get used a lot in models
-using DynamicPPL: @model, to_submodel
-using Distributions
-using DistributionsAD: filldist, arraydist
-using LinearAlgebra
+# function f(x::AbstractVector)
+#     logp = Ref(0.0)
+#     ts = broadcast(g, x)
+#     logp[] += ts[1][1]
+#     return logp[]
+# end
 
-include("models/assume_beta.jl")
-include("models/assume_dirichlet.jl")
-include("models/assume_lkjcholu.jl")
-include("models/assume_mvnormal.jl")
-include("models/assume_normal.jl")
-include("models/assume_submodel.jl")
-include("models/assume_wishart.jl")
-include("models/control_flow.jl")
-include("models/demo_assume_dot_observe.jl")
-include("models/demo_assume_dot_observe_literal.jl")
-include("models/demo_assume_index_observe.jl")
-include("models/demo_assume_matrix_observe_matrix_index.jl")
-include("models/demo_assume_multivariate_observe.jl")
-include("models/demo_assume_multivariate_observe_literal.jl")
-include("models/demo_assume_observe_literal.jl")
-include("models/demo_assume_submodel_observe_index_literal.jl")
-include("models/demo_dot_assume_observe.jl")
-include("models/demo_dot_assume_observe_index.jl")
-include("models/demo_dot_assume_observe_index_literal.jl")
-include("models/demo_dot_assume_observe_matrix_index.jl")
-include("models/demo_dot_assume_observe_submodel.jl")
-include("models/dot_assume.jl")
-include("models/dot_observe.jl")
-include("models/dynamic_constraint.jl")
-include("models/multiple_constraints_same_var.jl")
-include("models/multithreaded.jl")
-include("models/n010.jl")
-include("models/n050.jl")
-include("models/n100.jl")
-include("models/n500.jl")
-include("models/observe_index.jl")
-include("models/observe_literal.jl")
-include("models/observe_multivariate.jl")
-include("models/observe_submodel.jl")
-include("models/pdb_eight_schools_centered.jl")
-include("models/pdb_eight_schools_noncentered.jl")
+function f(x::AbstractVector)
+    ts = broadcast(g, x)
+    return ts[1][1]
+end
 
-include("models/dppl_gauss_unknown.jl")
-include("models/dppl_high_dim_gauss.jl")
-include("models/dppl_naive_bayes.jl")
+x = [0.5, 1.0]
+# @show f(x)
 
-# The entry point to this script itself begins here
-if ARGS == ["--list-model-keys"]
-    foreach(println, sort(collect(keys(MODELS))))
-elseif ARGS == ["--list-adtype-keys"]
-    foreach(println, sort(collect(keys(ADTYPES))))
-elseif length(ARGS) == 3 && ARGS[1] == "--run"
-    model, adtype = MODELS[ARGS[2]], ADTYPES[ARGS[3]]
+Enzyme.gradient(Reverse, f, x)
 
-    try
-        if ARGS[2] == "control_flow"
-            # https://github.com/TuringLang/ADTests/issues/4
-            vi = DynamicPPL.unflatten(VarInfo(model), [0.5, -0.5])
-            params = [-0.5, 0.5]
-            result = run_ad(model, adtype; varinfo = vi, params = params, benchmark = true)
-        else
-            result = run_ad(model, adtype; benchmark = true)
-        end
-        # If reached here - nothing went wrong
-        println(result.time_vs_primal)
-    catch e
-        @show e
-        if e isa ADIncorrectException
-            # First check for completely incorrect ones
-            for (a, b) in zip(e.grad_expected, e.grad_actual)
-                if !isnan(a) && !isnan(b) && abs(a - b) > 1e-6
-                    println("wrong")
-                    exit()
-                end
-            end
-            # If not, check for NaN's and report those
-            if any(isnan, e.grad_expected) || any(isnan, e.grad_actual)
-                println("NaN")
-            else
-                # Something else went wrong, shouldn't happen
-                println("wrong")
-            end
-        else
-            # Some other error, just say it's an error
-            println("error")
-        end
-    end
-else
-    println("Usage: julia main.jl --list-model-keys")
-    println("       julia main.jl --list-adtype-keys")
-    println("       julia main.jl --run <model> <adtype>")
 end
--- models/dppl_naive_bayes.jl
+++ models/dppl_naive_bayes.jl
@@ -1,27 +1,36 @@
-using MLDatasets: MNIST
-using MultivariateStats: fit, PCA, transform
+module DPPLNaiveBayes
+
+# using DynamicPPL, Distributions
+# using DistributionsAD: filldist
+# using LinearAlgebra: I
+# using MLDatasets
+# using MultivariateStats: fit, PCA, transform
 
 # Load MNIST images and labels
-features = MNIST(split=:train).features
-nrows, ncols, nimages = size(features)
-image_raw = Float64.(reshape(features, (nrows * ncols, nimages)))
-labels = MNIST(split=:train).targets .+ 1
-C = 10 # Number of labels
+# features = MNIST(split=:train).features
+# nrows, ncols, nimages = size(features)
+# image_raw = Float64.(reshape(features, (nrows * ncols, nimages)))
+# labels = MNIST(split=:train).targets .+ 1
+# C = 10 # Number of labels
 
-# Preprocess the images by reducing dimensionality
-D = 40
-pca = fit(PCA, image_raw; maxoutdim=D)
-image = transform(pca, image_raw)
+# # Preprocess the images by reducing dimensionality
+# D = 40
+# pca = fit(PCA, image_raw; maxoutdim=D)
+# image = transform(pca, image_raw)
 
-# Take only the first 1000 images and vectorise
-N = 1000
-image_subset = image[:, 1:N]'
-image_vec = vec(image_subset[:, :])
-labels = labels[1:N]
+# # Take only the first 1000 images and vectorise
+# N = 1000
+# image_subset = image[:, 1:N]'
+# image_vec = vec(image_subset[:, :])
+# labels = labels[1:N]
 
-@model dppl_naive_bayes(image_vec, labels, C, D) = begin
-    m ~ filldist(Normal(0, 10), C, D)
-    image_vec ~ MvNormal(vec(m[labels, :]), I)
+# @model function dppl_naive_bayes(image_vec, labels, C, D)
+#     m ~ filldist(Normal(0, 10), C, D)
+#     image_vec ~ MvNormal(vec(m[labels, :]), I)
+# end
+
+# model = dppl_naive_bayes(image_vec, labels, C, D)
+
 end
 
-@register dppl_naive_bayes(image_vec, labels, C, D)
+# @register DPPLNaiveBayes.model
