8bc8c5e77b1d27fb0d1b378de658ed4d67d7df75
Peter Thestrup Waade <peter@waade.net>
2025-07-22 09:03:04 +0200

    removed C library naming

--- models/call_C.jl
+++ models/call_C.jl
@@ -1,13 +1,8 @@
-# Get name of C standard library depending on the platform
-libc_name = Sys.iswindows() ? "msvcrt.dll" :
-            Sys.isapple()   ? "libc.dylib" :
-                              "libc.so.6"
-
 @model function call_C(y = 0.0)
     x ~ Normal(0, 1)
 
     # Call C library abs function
-    x_abs = @ccall libc_name.fabs(x::Cdouble)::Cdouble
+    x_abs = @ccall fabs(x::Cdouble)::Cdouble
 
     y ~ Normal(0, x_abs)
 end
