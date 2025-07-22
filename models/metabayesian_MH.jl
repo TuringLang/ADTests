18d32a8aefca8a58351d5735cb2ec59126672c1a
Penelope Yong <penelopeysm@gmail.com>
2025-07-22 18:25:17 +0100

    remove extra tilde

--- models/metabayesian_MH.jl
+++ models/metabayesian_MH.jl
@@ -6,7 +6,7 @@ Here we use a Metropolis-Hasting sampler implemented with Turing as the inversio
 
 # Inner model function
 @model function inner_model(observation, prior_μ = 0, prior_σ = 1)
-    
+
     # The innter model's prior
     mean ~ Normal(prior_μ, prior_σ)
 
@@ -15,16 +15,21 @@ Here we use a Metropolis-Hasting sampler implemented with Turing as the inversio
 end
 
 # Outer model function
-@model function metabayesian_MH(observation, action, inner_sampler = MH(), inner_n_samples = 20)
-    
+@model function metabayesian_MH(
+    observation,
+    action,
+    inner_sampler = MH(),
+    inner_n_samples = 20,
+)
+
     ### Sample parameters for the inner inference and response ###
 
     #The inner model's prior's sufficient statistics
     subj_prior_μ ~ Normal(0, 1)
-    subj_prior_σ = 1.0 
-    
+    subj_prior_σ = 1.0
+
     # #Inverse temperature for actions
-    β ~ 1
+    β = 1
 
     ### "Perceptual inference": running the inner model ###
 
