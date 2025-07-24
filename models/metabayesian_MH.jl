7f58934846bbf506ecfbd54bd4f8ba8883c2c3df
Penelope Yong <penelopeysm@gmail.com>
2025-07-24 11:43:41 +0100

    Seed inner sampling to ensure reproducibility of gradients

--- models/metabayesian_MH.jl
+++ models/metabayesian_MH.jl
@@ -3,13 +3,12 @@ This is a "meta-Bayesian" model, where the generative model includes an inversio
 These types of models are common in cognitive modelling, where systems of interest (e.g. human subjects) are thought to use Bayesian inference to navigate their environment.
 Here we use a Metropolis-Hasting sampler implemented with Turing as the inversion of the inner "subjective" model.
 =#
+using Random: Xoshiro
 
 # Inner model function
 @model function inner_model(observation, prior_μ = 0, prior_σ = 1)
-
-    # The innter model's prior
+    # The inner model's prior
     mean ~ Normal(prior_μ, prior_σ)
-
     # The inner model's likelihood
     observation ~ Normal(mean, 1)
 end
@@ -21,33 +20,25 @@ end
     inner_sampler = MH(),
     inner_n_samples = 20,
 )
-
     ### Sample parameters for the inner inference and response ###
-
-    #The inner model's prior's sufficient statistics
+    # The inner model's prior's sufficient statistics
     subj_prior_μ ~ Normal(0, 1)
     subj_prior_σ = 1.0
-
-    # #Inverse temperature for actions
+    # Inverse temperature for actions
     β = 1
 
     ### "Perceptual inference": running the inner model ###
-
-    #Condition the inner model
+    # Condition the inner model
     inner_m = inner_model(observation, subj_prior_μ, subj_prior_σ)
-
-    #Run the inner Bayesian inference
-    chns = sample(inner_m, inner_sampler, inner_n_samples, progress = false)
-
-    #Extract subjective point estimate
+    # Run the inner Bayesian inference
+    chns = sample(Xoshiro(468), inner_m, inner_sampler, inner_n_samples, progress = false)
+    # Extract subjective point estimate
     subj_mean_expectationₜ = mean(chns[:mean])
 
 
     ### "Response model": picking an action ###
-
-    #The action is a Gaussian-noise report of the subjective point estimate
+    # The action is a Gaussian-noise report of the subjective point estimate
     action ~ Normal(subj_mean_expectationₜ, β)
-
 end
 
 model = metabayesian_MH(0.0, 1.0)
