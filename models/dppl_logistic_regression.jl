16a1da60b91ccca86234833c4e6719a8dbe5d5ed
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-08-14 21:26:11 +0100

    Update dppl_logistic_regression.jl

--- models/dppl_logistic_regression.jl
+++ models/dppl_logistic_regression.jl
@@ -1,5 +1,9 @@
 using StatsFuns: logistic
 
+function safelogistic(x::T) where {T}
+    logistic(x) * (1 - 2 * eps(T)) + eps(T)
+end
+
 d, n = 100, 10_000
 X = randn(d, n)
 w = randn(d)
@@ -9,7 +13,7 @@ y = Int.(logistic.(X' * w) .> 0.5)
 @model function dppl_logistic_regression(Xt, y)
     N, D = size(Xt)
     w ~ product_distribution(Normal.(zeros(D)))
-    y ~ product_distribution(Bernoulli.(logistic.(Xt * w)))
+    y ~ product_distribution(Bernoulli.(safelogistic.(Xt * w)))
 end
 
 model = dppl_logistic_regression(X', y)
